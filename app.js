e6d458061d148bc32a8b15389364984114c0bf06
derfea <42876528+chikamso@users.noreply.github.com>
2020-02-22 19:15:57 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const app = () => {
+    const song = document.querySelector('song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
+
+    //Sounds
+    const sounds = document.querySelectorAll('.sound-picker button');
+    //Time display
+    const timeDisplay = document.querySelector('.time-display');
+    //get the length of the outline
+    const outlineLength = outline.getTotalLength();
+    console.log(outlineLength);
+    //Duration
+    let fakeDuration = 600;
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDasharray = outlineLength;
+}
